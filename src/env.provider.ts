6804e2bce9c02fb31d75e40176564b2759c7489a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-02-16 22:51:26 +0800

    fix: env coerce to number

--- src/env.provider.ts
+++ src/env.provider.ts
@@ -4,9 +4,9 @@ import { z } from "zod";
 const envSchema = z.object({
     SERVER_PORT: z.coerce.number(),
     SERVER_API_KEY: z.string(),
-    L1_CHAIN_ID: z.string(),
+    L1_CHAIN_ID: z.coerce.number(),
     L1_RPC: z.string(),
-    L2_CHAIN_ID: z.string(),
+    L2_CHAIN_ID: z.coerce.number(),
     L2_RPC: z.string(),
 });
 
