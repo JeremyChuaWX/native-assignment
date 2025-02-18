cce15fe5606038baee710dd116ea56ebacbf3406
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-02-18 09:15:00 +0800

    chore: rename files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post, UsePipes } from "@nestjs/common";
 import { BridgeService } from "src/bridge.service";
-import { Validate } from "src/validation";
+import { Validate } from "src/validation.pipe";
 import { z } from "zod";
 import {
     depositERC20Schema,
     depositETHSchema,
     withdrawERC20Schema,
     withdrawETHSchema,
-} from "./app.validation";
+} from "./app.schemas";
 
 @Controller()
 export class AppController {
@@ -16,24 +16,36 @@ export class AppController {
     @Post("/eth/deposit")
     @UsePipes(new Validate(depositETHSchema))
     async depositETH(@Body() body: z.infer<typeof depositETHSchema>) {
-        await this.bridgeService.depositETH(body);
+        const transaction = await this.bridgeService.depositETH(body);
+        return {
+            transaction: transaction.transactionHash,
+        };
     }
 
     @Post("/eth/withdraw")
     @UsePipes(new Validate(withdrawETHSchema))
     async withdrawETH(@Body() body: z.infer<typeof withdrawETHSchema>) {
-        await this.bridgeService.withdrawETH(body);
+        const transaction = await this.bridgeService.withdrawETH(body);
+        // return {
+        //     transaction: transaction.transactionHash,
+        // };
     }
 
     @Post("/erc20/deposit")
     @UsePipes(new Validate(depositERC20Schema))
     async depositERC20(@Body() body: z.infer<typeof depositERC20Schema>) {
-        await this.bridgeService.depositERC20(body);
+        const transaction = await this.bridgeService.depositERC20(body);
+        // return {
+        //     transaction: transaction.transactionHash,
+        // };
     }
 
     @Post("/erc20/withdraw")
     @UsePipes(new Validate(withdrawERC20Schema))
     async withdrawERC20(@Body() body: z.infer<typeof withdrawERC20Schema>) {
-        await this.bridgeService.withdrawERC20(body);
+        const transaction = await this.bridgeService.withdrawERC20(body);
+        // return {
+        //     transaction: transaction.transactionHash,
+        // };
     }
 }
