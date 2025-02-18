a063b606eea7b4ef07fe3a15411687643dd0c10c
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-02-18 09:14:39 +0800

    feat: bridge service

--- src/bridge.service.ts
+++ src/bridge.service.ts
@@ -1,4 +1,8 @@
-import { CrossChainMessenger, MessageStatus } from "@mantleio/sdk";
+import {
+    CrossChainMessenger,
+    DEFAULT_L2_CONTRACT_ADDRESSES,
+    MessageStatus,
+} from "@mantleio/sdk";
 import { Inject, Injectable } from "@nestjs/common";
 import { ethers } from "ethers";
 import type { Env } from "./env.provider";
@@ -26,54 +30,92 @@ export class BridgeService {
         });
     }
 
-    async depositETH(payload: {
-        l1PrivateKey: string;
-        l2PublicKey: string;
-        amount: string;
-    }) {
+    async depositETH(payload: { privateKey: string; amount: string }) {
         const l1Wallet = new ethers.Wallet(
-            payload.l1PrivateKey,
+            payload.privateKey,
             this.l1RpcProvider,
         );
-        const response = await this.messenger.depositETH(payload.amount, {
-            recipient: payload.l2PublicKey,
-            signer: l1Wallet,
-        });
-        await response.wait();
-        await this.messenger.waitForMessageStatus(
-            response,
-            MessageStatus.RELAYED,
-        );
-    }
-
-    async withdrawETH(payload: {
-        l1PublicKey: string;
-        l2PrivateKey: string;
-        amount: string;
-    }) {
         const l2Wallet = new ethers.Wallet(
-            payload.l2PrivateKey,
+            payload.privateKey,
             this.l2RpcProvider,
         );
-        const response = await this.messenger.withdrawETH(payload.amount, {
-            recipient: payload.l1PublicKey,
-            signer: l2Wallet,
+        const l2Address = await l2Wallet.getAddress();
+
+        console.log("Deposit ETH");
+        const response = await this.messenger.depositETH(payload.amount, {
+            recipient: l2Address,
+            signer: l1Wallet,
         });
+
+        console.log(`Transaction hash (on L1): ${response.hash}`);
+        const receipt = await response.wait();
+
+        /*
+        // BUG: waitForMessageStatus searches from genesis block to latest, rate limited by RPC
+
+        console.log("Waiting for status to change to RELAYED");
+        await this.messenger.waitForMessageStatus(
+            response.hash,
+            MessageStatus.RELAYED,
+        );
+        */
+
+        return receipt;
+    }
+
+    async withdrawETH(payload: { privateKey: string; amount: string }) {
+        const l1Wallet = new ethers.Wallet(
+            payload.privateKey,
+            this.l1RpcProvider,
+        );
+        const l2Wallet = new ethers.Wallet(
+            payload.privateKey,
+            this.l2RpcProvider,
+        );
+        const l1Address = await l1Wallet.getAddress();
+
+        const approve = await this.messenger.approveERC20(
+            ethers.constants.AddressZero,
+            DEFAULT_L2_CONTRACT_ADDRESSES.BVM_ETH,
+            payload.amount,
+            { signer: l2Wallet },
+        );
+        console.log(`Approve transaction hash (on L2): ${approve.hash}`);
+        const response = await this.messenger.withdrawERC20(
+            ethers.constants.AddressZero,
+            DEFAULT_L2_CONTRACT_ADDRESSES.BVM_ETH,
+            payload.amount,
+            {
+                recipient: l1Address,
+                signer: l2Wallet,
+            },
+        );
+        console.log(`Transaction hash (on L2): ${response.hash}`);
         await response.wait();
+        console.log("Waiting for status to be READY_TO_PROVE");
         await this.messenger.waitForMessageStatus(
             response.hash,
             MessageStatus.READY_TO_PROVE,
         );
         await this.messenger.proveMessage(response.hash);
+
+        console.log("Waiting for status to change to IN_CHALLENGE_PERIOD");
         await this.messenger.waitForMessageStatus(
             response.hash,
             MessageStatus.IN_CHALLENGE_PERIOD,
+        );
+
+        console.log(
+            "In the challenge period, waiting for status READY_FOR_RELAY",
         );
         await this.messenger.waitForMessageStatus(
             response.hash,
             MessageStatus.READY_FOR_RELAY,
         );
+        console.log("Ready for relay, finalizing message now");
         await this.messenger.finalizeMessage(response.hash);
+
+        console.log("Waiting for status to change to RELAYED");
         await this.messenger.waitForMessageStatus(
             response,
             MessageStatus.RELAYED,
@@ -81,14 +123,13 @@ export class BridgeService {
     }
 
     async depositERC20(payload: {
+        privateKey: string;
         l1TokenAddress: string;
-        l1PrivateKey: string;
         l2TokenAddress: string;
-        l2PublicKey: string;
         amount: string;
     }) {
         const l1Wallet = new ethers.Wallet(
-            payload.l1PrivateKey,
+            payload.privateKey,
             this.l1RpcProvider,
         );
         const allowanceResponse = await this.messenger.approveERC20(
@@ -105,7 +146,7 @@ export class BridgeService {
             payload.l2TokenAddress,
             payload.amount,
             {
-                recipient: payload.l2PublicKey,
+                recipient: payload.privateKey,
                 signer: l1Wallet,
             },
         );
@@ -117,14 +158,13 @@ export class BridgeService {
     }
 
     async withdrawERC20(payload: {
+        privateKey: string;
         l1TokenAddress: string;
-        l1PublicKey: string;
         l2TokenAddress: string;
-        l2PrivateKey: string;
         amount: string;
     }) {
         const l2Wallet = new ethers.Wallet(
-            payload.l2PrivateKey,
+            payload.privateKey,
             this.l2RpcProvider,
         );
         const response = await this.messenger.withdrawERC20(
@@ -132,7 +172,7 @@ export class BridgeService {
             payload.l2TokenAddress,
             payload.amount,
             {
-                recipient: payload.l1PublicKey,
+                recipient: payload.privateKey,
                 signer: l2Wallet,
             },
         );
